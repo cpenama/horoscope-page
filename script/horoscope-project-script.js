e776a47a2fd1bb9afb01b34dce6e45629b9b0932
Celia P <cpm1596@gmail.com>
2024-03-23 22:51:30 +0100

    changed number strings to numbers

--- script/horoscope-project-script.js
+++ script/horoscope-project-script.js
@@ -9,43 +9,86 @@ function assignZodiacSign() {
 	console.log('Month ', valueMonth);
 	console.log('Day ', valueDay)
 
-	if ((valueMonth === '3' && valueDay >= '21') || 
-    (valueMonth === '4' && valueDay <= '19')) {
+	if ((valueMonth === 'March' && valueDay >= 21) || 
+    (valueMonth === 'April' && valueDay <= 19)) {
 		document.querySelector('.js-horoscope-reveal').innerHTML = '♈ Aries';
 	}
-	else if ((valueMonth === '4' && valueDay >= '20') || (valueMonth === '5' && valueDay <= '20')) {
+	else if ((valueMonth === 'April' && valueDay >= 20) || (valueMonth === 'May' && valueDay <= 20)) {
 		document.querySelector('.js-horoscope-reveal').innerHTML = '♉ Taurus';
 	}
-	else if ((valueMonth === '5' && valueDay >= '21') || (valueMonth === '6' && valueDay <= '20')) {
+	else if ((valueMonth === 'May' && valueDay >= 21) || (valueMonth === 'June' && valueDay <= 20)) {
 		document.querySelector('.js-horoscope-reveal').innerHTML = '♊ Gemini';
 	}
-	else if ((valueMonth === '6' && valueDay >= '21') || (valueMonth === '7' && valueDay <= '22')) {
+	else if ((valueMonth === 'June' && valueDay >= 21) || (valueMonth === 'July' && valueDay <= 22)) {
 		document.querySelector('.js-horoscope-reveal').innerHTML = '♋ Cancer';
 	}
-	else if ((valueMonth === '7' && valueDay >= '23') || (valueMonth === '8' && valueDay <= '22')) {
+	else if ((valueMonth === 'July' && valueDay >= 23) || (valueMonth === 'August' && valueDay <= 22)) {
 		document.querySelector('.js-horoscope-reveal').innerHTML = '♌ Leo';
 	}
-	else if ((valueMonth === '8' && valueDay >= '23') || (valueMonth === '9' && valueDay <= '22')) {
+	else if ((valueMonth === 'August' && valueDay >= 23) || (valueMonth === 'September' && valueDay <= 22)) {
 		document.querySelector('.js-horoscope-reveal').innerHTML = '♍ Virgo';
 	}
-	else if ((valueMonth === '9' && valueDay >= '23') || (valueMonth === '10' && valueDay <= '22')) {
+	else if ((valueMonth === 'September' && valueDay >= 23) || (valueMonth === 'October' && valueDay <= 22)) {
 		document.querySelector('.js-horoscope-reveal').innerHTML = '♎ Libra';
 	}
-	else if ((valueMonth === '10' && valueDay >= '23') || (valueMonth === '11' && valueDay <= '21')) {
+	else if ((valueMonth === 'October' && valueDay >= 23) || (valueMonth === 'November' && valueDay <= 21)) {
 		document.querySelector('.js-horoscope-reveal').innerHTML = '♏ Scorpio';
 	}
-	else if ((valueMonth === '11' && valueDay >= '22') || (valueMonth === '12' && valueDay <= '21')) {
+	else if ((valueMonth === 'November' && valueDay >= 22) || (valueMonth === 'December' && valueDay <= 21)) {
 		document.querySelector('.js-horoscope-reveal').innerHTML = '♐ Sagittarius';
 	}
-	else if ((valueMonth === '12' && valueDay >= '22') || (valueMonth === '1' && valueDay <= '19')) {
+	else if ((valueMonth === 'December' && valueDay >= 22) || (valueMonth === 'January' && valueDay <= 19)) {
 		document.querySelector('.js-horoscope-reveal').innerHTML = '♑ Capricorn';
 	}
-	else if ((valueMonth === '1' && valueDay >= '20') || (valueMonth === '2' && valueDay <= '18')) {
+	else if ((valueMonth === 'January' && valueDay >= 20) || (valueMonth === 'February' && valueDay <= 18)) {
 		document.querySelector('.js-horoscope-reveal').innerHTML = '♒ Aquarius';
 	}
-	else if ((valueMonth === '2' && valueDay >= '19') || (valueMonth === '3' && valueDay <= '20')) {
+	else if ((valueMonth === 'February' && valueDay >= 19) || (valueMonth === 'March' && valueDay <= 20)) {
 		document.querySelector('.js-horoscope-reveal').innerHTML = '♓ Piscis';
 	}
+
+	const starSign = document.querySelector('.js-horoscope-reveal').innerHTML;
+	
+	let signDescription = '';
+
+	if (starSign === '♈ Aries') {
+		signDescription = 'Energetic and assertive, Aries are natural leaders known for their enthusiasm and determination.';
+	}
+	else if (starSign === '♉ Taurus') {
+		signDescription = 'Reliable and practical, Taurus individuals are grounded and enjoy stability and comfort in life.'
+	}
+	else if (starSign === '♊ Gemini') {
+		signDescription = 'Versatile and curious, Geminis are social butterflies who thrive on communication and intellectual stimulation.'
+	}
+	else if (starSign === '♋ Cancer') {
+		signDescription = 'Nurturing and intuitive, Cancers are highly sensitive individuals who value emotional connections and security.'
+	}
+	else if (starSign === '♌ Leo') {
+		signDescription = 'Confident and charismatic, Leos are natural-born leaders who enjoy being in the spotlight and have a generous spirit.'
+	}
+	else if (starSign === '♍ Virgo') {
+		signDescription = 'Detail-oriented and analytical, Virgos are perfectionists who excel in organization and critical thinking.'
+	}
+	else if (starSign === '♎ Libra') {
+		signDescription = 'Diplomatic and charming, Libras are sociable individuals who value harmony and fairness in their relationships.'
+	}
+	else if (starSign === '♏ Scorpio') {
+		signDescription = 'Intense and passionate, Scorpios are fiercely loyal and known for their determination and emotional depth.'
+	}
+	else if (starSign === '♐ Sagittarius') {
+		signDescription = 'Adventurous and optimistic, Sagittarians are free spirits who enjoy exploring new ideas and cultures.'
+	}
+	else if (starSign === '♑ Capricorn') {
+		signDescription = 'Ambitious and disciplined, Capricorns are hardworking individuals who value tradition and strive for success.'
+	}
+	else if (starSign === '♒ Aquarius') {
+		signDescription = 'Independent and unconventional, Aquarians are forward-thinking individuals who champion humanitarian causes.'
+	}
+	else if (starSign === '♓ Piscis') {
+		signDescription = 'Imaginative and compassionate, Pisceans are intuitive souls who are deeply empathetic and creative.'
+	}
+	document.querySelectorAll('.js-horoscope-description').forEach(element => {element.innerHTML = signDescription;
+	});
 }
 
 document.getElementById('resetButton').addEventListener('click', function() {
